394ed60c2d0a75e119a6c550204a3cc6a1c86ec6
HP <HP@KALASHDL>
2024-01-26 23:03:34 +0530

    Add project files.

---
+++ Controllers/ManageController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISMLogin.Models;
+
+namespace ISMLogin.Controllers
+{
+    public class ManageController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Membership Model)
+        {
+            using (var context = new IMSEntities())
+            {
+                bool result = context.UserMasters.Any(x => x.Username == Model.Username && x.Password == Model.Password);  
+                if(result==true)
+                {
+                    return RedirectToAction("Create","TeacherMasters");
+                }
+                ModelState.AddModelError("", "Username and password are incorrect");
+            }
+            return RedirectToAction("Create", "TeacherMasters");
+
+        }
+
+        public ActionResult signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult signup(UserMaster Model)
+        {
+            using (var context = new IMSEntities())
+            {
+                context.UserMasters.Add(Model);
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Models/Membership.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace ISMLogin.Models
+{
+    public class Membership
+    {
+        public int id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
